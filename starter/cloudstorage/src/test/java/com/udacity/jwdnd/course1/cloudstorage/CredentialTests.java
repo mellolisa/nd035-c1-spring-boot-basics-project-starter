929cf00fdc2362fa54caf735230cd36236d36b68
mellolisa <39321631+mellolisa@users.noreply.github.com>
2020-07-17 22:40:07 -0400

    Automate signup and login for testing

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialTests.java
@@ -0,0 +1,83 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CredentialTests {
+
+    @LocalServerPort
+    private int port;
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        this.driver = new ChromeDriver();
+    }
+
+    /*
+    @AfterEach
+    public void afterEach() {
+        if (this.driver != null) {
+            driver.quit();
+        }
+    }
+    */
+
+    @Test
+    public void getLoginPage() {
+        driver.get("http://localhost:" + this.port + "/login");
+        Assertions.assertEquals("Super Duper Drive - Login", driver.getTitle());
+    }
+
+    @Test
+    public void credentialTests() throws InterruptedException {
+        //start the driver, open chrome to our target url
+        driver.get("http://localhost:" + this.port + "/signup");
+
+        //create a user for testing login
+        WebElement inputField = driver.findElement(By.id("inputFirstName"));
+        inputField.sendKeys("aa");
+
+        inputField = driver.findElement(By.id("inputLastName"));
+        inputField.sendKeys("aa");
+
+        inputField = driver.findElement(By.id("inputUsername"));
+        inputField.sendKeys("aaaaa");
+
+        inputField = driver.findElement(By.id("inputPassword"));
+        inputField.sendKeys("aaaaa");
+
+        inputField.submit();
+        Thread.sleep(1000);
+
+        //navigate to login page
+        driver.get("http://localhost:" + this.port + "/login");
+
+        //login success
+        inputField = driver.findElement(By.id("inputUsername"));
+        inputField.sendKeys("aaaaa");
+
+        inputField = driver.findElement(By.id("inputPassword"));
+        inputField.sendKeys("aaaaa");
+
+        inputField.submit();
+        Thread.sleep(1000);
+        
+    }
+
+}
